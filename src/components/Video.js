092bb8c2d2f526735d8cd59a8959a1bbde8be9b6
Boe Beito <bbeito@gmail.com>
2022-11-05 12:55:07 -0700

    created class names for CSS styling

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,9 +4,9 @@ import Duration from './Duration'
 const Video = (props) => {
   return (
     <li>
-      <h3>{props.video.title}</h3>
+      <h3 className="videoTitle">{props.video.title}</h3>
       <Duration duration={props.video.duration} />
-      <img src={props.video.thumbnail_path} />
+      <img className="thumbnail" src={props.video.thumbnail_path} />
       <Rating rating={props.video.rating} />
     </li>
   )
