74b2d19bcb5f0c376095f6dbe16730f35aa9ad93
Boe Beito <bbeito@gmail.com>
2022-11-06 12:04:20 -0800

    changed some margins and font style

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,5 +1,5 @@
 const Rating = (props) => {
-  return <h2 className="ratingValue">Rating: {props.rating}</h2>
+  return <h2 className="ratingValue">Rating: {props.rating}%</h2>
 }
 
 export default Rating
